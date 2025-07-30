d928473f681629deabe31d50094e3687b43614b1
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-07-30 18:26:31 -0500

    add spinner

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded',()=>{
 const formulario = document.getElementById('formulario')
 const inputFile = document.getElementById('inputFile')
 const boton = document.getElementById('boton')
+const loader = document.getElementById('loader')
+const containerLoader = document.querySelector('.container_loader')
 
 boton.addEventListener('click',()=>{
 inputFile.click()
@@ -22,7 +24,8 @@ const formData = new FormData()
 formData.append("file",file)
 
 try {
-
+containerLoader.style.visibility = 'visible'
+loader.style.visibility = 'visible' 
     const res = await fetch(`https://automatizacion-comprobantes.onrender.com/subir-excel`,{
         method:"POST",
        
@@ -31,7 +34,9 @@ try {
 
     const data = await res.json()
     alert("Archivo enviado con exito")
-    
+    containerLoader.style.visibility = 'hidden'
+    loader.style.visibility = 'hidden'
+
 } catch (error) {
     console.error('Error al enviar el archivo', error);
       alert('Hubo un error al enviar el archivo');
